8610b74c26a0bc93a44fbe0511a31bea081d846f
Shuangliang Chen <sc60@anduril.csl.illinois.edu>
2024-12-01 11:05:01 -0600

    v0.0.2

--- benchmark.py
+++ benchmark.py
@@ -1,75 +1,67 @@
-from llm import get_response
 import json
 import os
-import ast
 
-MAX=100000
+from datasets import load_dataset
+from llm import batch_inference
 
-def evaluate(model, cot=False, dataset='ARC-AGI/data/evaluation', max_testcases=MAX):
-    print(f'Evaluating {model}{" with CoT" if cot else ""}')
-    results = {}
-    system_prompt = 'You solve puzzles by transforming the input into the output. You are expert at the PGM (Portable Graymap) format.'
-    testcases = os.listdir(dataset)
-    testcases.sort()
-    testcases = testcases[:max_testcases]
-    for i, testcase in enumerate(testcases):
-        print(f'Evaluating {testcase} ({i+1}/{len(testcases)})')
-        with open(f'{dataset}/{testcase}') as f:
-            data = json.load(f)
-        user_prompt = f"""# Transformations
-## Transformation A - Input
-```
-{data['train'][0]["input"]}
-```
-## Transformation A - Output
-```
-{data['train'][0]["output"]}
-```
-## Transformation B - Input
-```
-{data['test'][0]["input"]}
-```
-## Transformation B - Output
-```
-# Predict this output
-```
-Wrap your output matrix in <Matrix> and </Matrix>.
+
+NUM_TEST_SAMPLES = 1000000
+
+def benchmark(model_id: str, dataset_id: str, use_cot: bool = False, batch_size: int = 4):
+    dataset = load_dataset("allenai/ai2_arc", dataset_id, split="test")
+    results = []
+
+    for i in range(0, min(NUM_TEST_SAMPLES, len(dataset)), batch_size):
+        prompts = []
+        for j in range(batch_size):
+            if i+j >= len(dataset):
+                break
+            entry = dataset[i+j]
+            system_prompt = f"You are an expert on solving complex problems."
+            cot_prompt = f"Think through the problem step by step." if use_cot else ""
+            prompt = f"""{system_prompt}
+Here is the question:
+{entry["question"]}
+{'\n'.join([label + ": " + text for text, label in zip(entry["choices"]["text"], entry["choices"]["label"])])}
+Wrap your answer in <ANSWER> and </ANSWER>. For example <ANSWER>A</ANSWER>.
+{cot_prompt}
 """
-        cot_prompt = 'Please think it through step by step.' if cot else ''
-        response = get_response(model, system_prompt, f"{user_prompt}{cot_prompt}")
-        is_correct = False
-        try:
-            prediction = response.split('<Matrix>')[1].split('</Matrix>')[0].strip()
-            prediction = ast.literal_eval(prediction)
-            golden = data['test'][0]["output"]
-            is_correct = prediction == golden
-            # if not is_correct:
-            #     print(f'Prediction: {prediction}')
-            #     print(f'Golden: {golden}')
-        except Exception as e:
-            # print(f'Error: {e}')
-            # print(f'Response: {response}')
-            pass
-
-        results[testcase] = {
-            'system_prompt': system_prompt,
-            'user_prompt': user_prompt,
-            'response': response,
-            'is_correct': is_correct
-        }
-        print(f'Correct: {is_correct}')
-
-    correct_count = sum([1 for result in results.values() if result['is_correct']])
-    print(f'Correct: {correct_count}/{len(results)} ({correct_count/len(results)*100:.2f}%)')
-    with open(f'evaluation_{model}_CoT={cot}.json', 'w') as f:
-        json.dump(results, f, indent=4)
+            prompts.append(prompt)
+        responses = batch_inference(model_id, prompts)
+        for j in range(batch_size):
+            if i+j >= len(dataset):
+                break
+            entry = dataset[i+j]
+            response = responses[j]
+            is_correct = False
+            try:
+                predicted = response.split("<ANSWER>")[1].split("</ANSWER>")[0].strip()
+                is_correct = predicted[0] == entry["answerKey"]
+                results.append(
+                    {
+                        "id": entry["id"],
+                        "question": entry["question"],
+                        "answer": entry["answerKey"],
+                        "predicted": predicted,
+                        "is_correct": is_correct,
+                    }
+                )
+                print(f"Testcase {i+j}: {'Correct' if is_correct else 'Incorrect'}")
+            except Exception as e:
+                print(f"Error: {e}")
+    print(f"Accuracy: {sum([1 for result in results if result['is_correct']]) / len(results)}")
+    json.dump(results, open(f"{dataset_id}-{model_id.split('/')[1]}-CoT-{use_cot}.json", "w"), indent=2)
     return results
 
-def run():
-    evaluate('llama3.1:8b')
-    evaluate('llama3.1:8b', cot=True)
-    evaluate('llama3.1:70b')
-    evaluate('llama3.1:70b', cot=True)
+
+        
+
+
+def main():
+    benchmark(model_id="meta-llama/Llama-3.1-8B-Instruct", dataset_id="ARC-Challenge", use_cot=False, batch_size=64)
+    benchmark(model_id="meta-llama/Llama-3.1-8B-Instruct", dataset_id="ARC-Challenge", use_cot=True, batch_size=64)
+    benchmark(model_id="meta-llama/Llama-3.1-70B-Instruct", dataset_id="ARC-Challenge", use_cot=False, batch_size=16)
+    benchmark(model_id="meta-llama/Llama-3.1-70B-Instruct", dataset_id="ARC-Challenge", use_cot=True, batch_size=16)
 
 if __name__ == '__main__':
-    run()
+    main()
